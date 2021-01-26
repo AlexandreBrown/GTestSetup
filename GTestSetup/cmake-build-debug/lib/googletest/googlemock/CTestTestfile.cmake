ef0747b15bf732309ec144fa8084c6df6cbf1bbd
Alexandre Brown <alexandre.brown@microdrones.com>
2021-01-26 00:46:58 -0500

    Added CTest & Testing Flags to CMakeLists

--- GTestSetup/cmake-build-debug/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/brownthedev/Documents/GTestSetup/GTestSetup/lib/googletest/googlemock
-# Build directory: /home/brownthedev/Documents/GTestSetup/GTestSetup/cmake-build-debug/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
