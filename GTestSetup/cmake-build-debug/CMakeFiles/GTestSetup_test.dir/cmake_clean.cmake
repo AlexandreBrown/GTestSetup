b590504dddb5e7e6f64b4f28c3020850e1dbf2d5
Alexandre Brown <alexandre.brown@microdrones.com>
2021-02-14 19:47:54 -0500

    Removed unneeded code in CMake

---
+++ GTestSetup/cmake-build-debug/CMakeFiles/GTestSetup_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GTestSetup_test.dir/src/main/MyClass.cpp.o"
+  "CMakeFiles/GTestSetup_test.dir/src/test/MyClassTest.cpp.o"
+  "GTestSetup_test"
+  "GTestSetup_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GTestSetup_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ GTestSetup/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/brownthedev/Documents/GTestSetup/GTestSetup
+# Build directory: /home/brownthedev/Documents/GTestSetup/GTestSetup/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(GTestSetup_test "GTestSetup_test")
+set_tests_properties(GTestSetup_test PROPERTIES  _BACKTRACE_TRIPLES "/home/brownthedev/Documents/GTestSetup/GTestSetup/CMakeLists.txt;28;add_test;/home/brownthedev/Documents/GTestSetup/GTestSetup/CMakeLists.txt;0;")
+subdirs("lib/googletest")
